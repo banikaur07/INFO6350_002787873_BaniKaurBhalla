31c9cf4de9258c2e8c24b928785b1dfa5d781c13
banikaur07 <banibhalla01@gmail.com>
2024-04-23 01:53:17 -0700

    Changes to exercise6a

--- exercises/exercise6a/lib/screens/home_screen.dart
+++ exercises/exercise6a/lib/screens/home_screen.dart
@@ -15,7 +15,7 @@ class HomeScreen extends StatelessWidget {
     final screenHeight = MediaQuery.sizeOf(context).height;
     const padding = EdgeInsets.symmetric(horizontal: 25, vertical: 30);
     const decoration = BoxDecoration(
-        color: Color.fromARGB(255, 170, 174, 183),
+        color: Color.fromARGB(255, 181, 187, 199),
         borderRadius: BorderRadius.vertical(top: Radius.circular(30)));
 
     return Consumer<CalculatorProvider>(builder: (context, provider, _) {
